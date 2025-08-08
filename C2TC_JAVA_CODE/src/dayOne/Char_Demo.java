cffc745bdb6752608dbd1d9360d1a1ff39209629
XR-official <xrecruit.official@gmail.com>
2025-08-08 13:24:31 +0530

    UPLOADING PARTIAL DAYONE CODE

---
+++ C2TC_JAVA_CODE/src/dayOne/Char_Demo.java
@@ -0,0 +1,31 @@
+package dayOne;
+
+
+public class Char_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a = 'a';
+		System.out.println(a);
+
+		// assigning number to char
+		char b = 65;
+		System.out.println(b);
+
+		// assigning unicode to char
+		char c = '\u00B2';
+		System.out.println(c);
+
+		// Unicode representation
+		char d = '\u10BA';
+		System.out.println(d);
+
+		// ASCI code representation
+		int e = 'A';
+		System.out.println(e);
+		
+		//Push Checking
+		System.out.println("Did we won ??");
+	}
+
+}
