1c798a89b2823eb454f329595e88ea49ff4b7092
XR-official <xrecruit.official@gmail.com>
2025-08-14 14:29:23 +0530

    RE COMMIT

--- C2TC_JAVA_CODE/src/dayThree/entity/Product_Demo.java
+++ C2TC_JAVA_CODE/src/dayThree/entity/Product_Demo.java
@@ -9,6 +9,17 @@ public class Product_Demo {
 		
 		Scanner sc=new Scanner(System.in);
 		
-		System.out.println("Enter the product details ");
+		System.out.println("Enter the product details : ");
+		
+		int sid=sc.nextInt();
+		sc.nextLine();
+		String prod_name=sc.nextLine();
+		float price = sc.nextFloat();
+		
+		Product o3=new Product(sid,prod_name,price);
+		
+		System.out.println(o1);
+		System.out.println(o2);
+		System.out.println(o3);
 	}
 }
