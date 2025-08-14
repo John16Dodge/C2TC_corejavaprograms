3e543dc80fd8b19c0f2ba13a50341381c11b9fcc
XR-official <xrecruit.official@gmail.com>
2025-08-14 14:29:10 +0530

    Day 3 Entity is added

---
+++ C2TC_JAVA_CODE/src/dayThree/entity/Product_Demo.java
@@ -0,0 +1,14 @@
+package dayThree.entity;
+
+import java.util.Scanner;
+
+public class Product_Demo {
+	public static void main(String[] args) {
+		Product o1=new Product();
+		Product o2=new Product(102,"mouse",250.4f);
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the product details ");
+	}
+}
