64e3fff1a9e830818be8ffc98032c892faf8fe0b
XR-official <xrecruit.official@gmail.com>
2025-08-18 13:36:46 +0530

    Day 2 partial programs

---
+++ C2TC_JAVA_CODE/src/dayTwo/IfElseDemo.java
@@ -0,0 +1,15 @@
+package dayTwo;
+
+public class IfElseDemo {
+	public static void main(String[] args) {
+		int age=11;
+		
+		if(age>=18)
+			System.out.println(" You are eligible for Voting");	
+		else
+			System.out.println("Sorry!, wait ");
+
+		System.out.println("if else demonstration");
+		
+	}
+}
