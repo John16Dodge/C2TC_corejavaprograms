2487daf9dd42ee8d76e2b9747bd50ff882d8d3c2
XR-official <xrecruit.official@gmail.com>
2025-08-19 14:03:46 +0530

    Inheritance

---
+++ C2TC_JAVA_CODE/src/JavaConceptExample/Inheritance/Single/ExecutionDemo.java
@@ -0,0 +1,8 @@
+package JavaConceptExample.Inheritance.Single;
+
+public class ExecutionDemo {
+	public static void main(String[] args) {
+		Trainer t = new Trainer(13,"Sanjay","Puducherry",81,"Pro","TechHub");
+		System.out.println(t);
+	}
+}
