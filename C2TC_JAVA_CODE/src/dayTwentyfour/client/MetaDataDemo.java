12cbd76e8acd6c1b30caa840a5c772ecb7839fea
XR-official <xrecruit.official@gmail.com>
2025-08-29 15:43:03 +0530

    day 24

---
+++ C2TC_JAVA_CODE/src/dayTwentyfour/client/MetaDataDemo.java
@@ -0,0 +1,16 @@
+package dayTwentyfour.client;
+
+//Program to demonstrate Database MetaData and ResultSet Metadata
+//package com.tnsif.daytwentyfour.client;
+
+import com.tnsif.daytwentyfour.metadata.DBMetadata;
+
+public class MetaDataDemo {
+
+	public static void main(String[] args) {
+		DBMetadata.showDatabaseMetataData();
+		DBMetadata.showRSMetadata();
+
+	}
+
+}
---
+++ C2TC_JAVA_CODE/src/dayTwentyfour/d.java
@@ -0,0 +1,5 @@
+package dayTwentyfour;
+
+public class d {
+
+}
