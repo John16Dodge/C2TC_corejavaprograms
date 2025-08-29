7bab9e94f098496bf5b56a5aa21b9e7744844a03
XR-official <xrecruit.official@gmail.com>
2025-08-29 15:38:25 +0530

    day 23

---
+++ C2TC_JAVA_CODE/src/dayTwentyThree/client/Executor.java
@@ -0,0 +1,24 @@
+package dayTwentyThree.client;
+
+import dayTwentyTwo.preparedstatementinterface.PreparedStatementDemo;
+
+//Program to demonstrate PreparedStatement Interface
+//package com.tnsif.twentythree.client;
+
+//import com.tnsif.daytwentytwo.preparedstatementinterface.PreparedStatementDemo;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		PreparedStatementDemo.showEmp();
+		PreparedStatementDemo.updateEmpName(3, "Neha");
+		PreparedStatementDemo.updateEmpSalary(3, 65000);
+		PreparedStatementDemo.showEmp();
+		if(!PreparedStatementDemo.deleteEmp(8));
+			System.err.println("No such employee...");
+	
+		PreparedStatementDemo.showEmp();
+
+	}
+
+}
---
+++ C2TC_JAVA_CODE/src/dayTwentyThree/e.java
@@ -0,0 +1,5 @@
+package dayTwentyThree;
+
+public class e {
+
+}
